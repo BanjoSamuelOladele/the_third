36b7f2b75e18a0b9241edc7ad606058ce3d5fbad
whitewizardd <o.samuel@native.semicolon.africa>
2024-02-25 12:05:10 +0100

    updated

--- contracts/WizardToken.sol
+++ contracts/WizardToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Wizard is ERC20, ERC20Burnable, Ownable {
+contract WizardToken is ERC20, ERC20Burnable, Ownable {
     constructor()
         ERC20("Wizz", "WTK")
         Ownable(msg.sender)
